b23290ba2b7739b4c00be8e8ff726d8f03f94507
Samuel Calderon <samuelcalderonsoto@gmail.com>
2025-07-25 07:02:22 -0500

    clase 24 july

---
+++ pythonClass/class25July-1.py
@@ -0,0 +1,19 @@
+"""
+Autor:
+Fecha:
+Descripcion:
+"""
+#Comentario en Linea
+"""
+Funciones:
+    -type: mostrar que tipo de dato es la variable de la cual se desea saber
+"""
+
+edad = 0
+estatura = float(0.0)
+
+#print - mostrar mensaje en consola
+print ( type (edad))
+
+#input - Ingresar un valor
+edad = int(input('Ingresa tu edad\n'))
