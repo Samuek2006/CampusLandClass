9f2c4c9e7c390e08ca8e72e943e2af3e1112debe
Samuel Calderon <samuelcalderonsoto@gmail.com>
2025-07-26 13:26:42 -0500

    taller 1

--- pythonHome/pythonSolucion/conversorTemperatura.py
+++ pythonHome/pythonSolucion/conversorTemperatura.py
@@ -4,9 +4,11 @@ Fecha: 25/07/2025
 Descripcion: Este Script un conversor de temperatura.
 """
 
-temperatura = float(input("Ingrese la temperatura: "))
-celFahrenheit = (temperatura * 9/5) + 32
-fahrCelsius = (temperatura - 32) * 5/9
-
-print(f"La temperatura en Fahrenheit es: {celFahrenheit:.2f} °F")
-print(f"La temperatura en Celsius es: {fahrCelsius:.2f} °C")
+temperatura = float(input('Ingrese la temperatura: '))
+unidadOrigen = input('Ingrese la unidad origen de temperatura:(c,f) ').upper()
+if (unidadOrigen == "C"):
+    conversion = (temperatura * 9/5) +32
+    print('El resultado de la conversion de Celsius a Fahrenheit es:', conversion)
+else:
+    conversion = (temperatura -32) * 5/9
+    print('El resultado de la conversion de Fahrenheit a Celsius es:', conversion)
