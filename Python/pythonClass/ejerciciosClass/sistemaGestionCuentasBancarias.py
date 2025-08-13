5d0640ff6413ecc3f1e1f488024fb8b7cb0b6ef8
Samuel Calderon <samuelcalderonsoto@gmail.com>
2025-08-13 06:17:14 -0500

    actualizacion taller

--- Python/pythonClass/ejerciciosClass/sistemaGestionCuentasBancarias.py
+++ Python/pythonClass/ejerciciosClass/sistemaGestionCuentasBancarias.py
@@ -47,15 +47,17 @@ ProductoClient
         - Valor
         - Tipo Movimiento
 """
-import os
+import os, random, time
 
 cuentasBancarias = {}
 
+#Funcuin Limpiar Consola
 def LimpiarConsola():
     limpiar = os.system("cls" if os.name == "nt" else "clear")
     return limpiar
 
-def addCuenta(numeroCuenta, titular, cc, correo, edad, movil, fijo, pais, dep, ciudad, direccion):
+#Funcion Agregar Cuenta
+def addCuenta(numeroCuenta, titular, cc, correo, edad, movil, fijo, pais, dep, ciudad, direccion,idProducto, producto, estado):
     if numeroCuenta in cuentasBancarias:
         print("La cuenta ya existe.")
     else:
@@ -74,58 +76,378 @@ def addCuenta(numeroCuenta, titular, cc, correo, edad, movil, fijo, pais, dep, c
                 "Ciudad": ciudad,
                 "Direccion": direccion
             },
-            "Productos": {},
-            "Historia": {}
+            "Productos": {
+                idProducto: {
+                    'NombreProducto' : producto,
+                    'Saldo': 0,
+                    'Estado': estado,
+                    'Historial' : {}
+                }
+            },
         }
     print("Cuenta creada exitosamente.")
 
 def addProducto():
     pass
+
+#Funcion Menu Clientes
+def menuClientes():
+    print('1. Datos Cliente \n2. Saldos \n3. Creditos \n4. Inversiones \n0.Salir')
+
+#Funcion Menu Saldos
+def menuSaldos():
+    print('1. Cta Ahorros \n2. Cta Corriente')
+
+#Funcion Menu Creditos
+def menuCreditos():
+    print('1. Credito Libre Inversion \n2. Credito Vivienda \n3. Credito Compra AutoMovil \n0. Salir ')
+
+#Funcion Menu Portafolio
+def menuPortafolio():
+    print('1. Cta Ahorros \n2. Cta Corriente \n3. CDT \n4. Credito Libre Inversion \n5. Credito Vivienda \n6. Credito Compra AutoMovil \n0. Salir')
+
 #Variables
 numeroCuenta = 4000
+ctaCorriente = 10000
+ctaAhorros = 80000
+cdt = 900000
+creditoLibreInv = 100000
+creditoVivienda = 500000
+creditoCompraAuto = 400000
+producto = ''
 
+#Ejecucion Principal
 while True:
     try:
-        opcion = int(input("Seleccione una opción:\n1. Crear cuenta\n2. Depositar\n3. Solicitar Crédito\n4. Retirar Dinero\n5. Pago Cuota Crédito\n6. Cancelar Cuenta\n0. Salir\n"))
+        opcion = int(input("Seleccione una opción:\n1. Crear cuenta\n2. Depositar\n3. Solicitar Crédito\n4. Retirar Dinero\n5. Pago Cuota Crédito\n6. Cancelar Cuenta\n0. Salir \nIngresa una Opcion: "))
         match opcion:
             case 1:
+                LimpiarConsola()
+
+                #Generacion Producto y eleccion de producto a solicitar
+                while True:
+                    menuPortafolio()
+                    opcion = input('\nIngresa una opcion valida: ')
+                    e = True
+                    idProducto = None  # <- inicializamos por defecto
+
+                    match opcion:
+                        case "1":
+                            producto = 'Cuenta Ahorros'
+                            while e:
+                                idProducto = random.randint(10000, ctaAhorros)
+                                existe = any(idProducto in cuenta["Productos"] for cuenta in cuentasBancarias.values())
+                                if not existe:
+                                    e = False
+                            break
+
+                        case "2":
+                            producto = 'Cta Corriente'
+                            while e:
+                                idProducto = random.randint(4000, ctaCorriente)
+                                existe = any(idProducto in cuenta["Productos"] for cuenta in cuentasBancarias.values())
+                                if not existe:
+                                    e = False
+                            break
+
+                        case "3":
+                            producto = 'CDT'
+                            while e:
+                                idProducto = random.randint(500000, cdt)
+                                existe = any(idProducto in cuenta["Productos"] for cuenta in cuentasBancarias.values())
+                                if not existe:
+                                    e = False
+                            break
+
+                        case "4":
+                            producto = 'Credito Libre Inversion'
+                            while e:
+                                idProducto = random.randint(80000, creditoLibreInv)
+                                existe = any(idProducto in cuenta["Productos"] for cuenta in cuentasBancarias.values())
+                                if not existe:
+                                    e = False
+                            break
+
+                        case "5":
+                            producto = 'Credito Vivienda'
+                            while e:
+                                idProducto = random.randint(400000, creditoVivienda)
+                                existe = any(idProducto in cuenta["Productos"] for cuenta in cuentasBancarias.values())
+                                if not existe:
+                                    e = False
+                            break
+
+                        case "6":
+                            producto = 'Credito Compra AutoMovil'
+                            while e:
+                                idProducto = random.randint(100000, creditoCompraAuto)
+                                existe = any(idProducto in cuenta["Productos"] for cuenta in cuentasBancarias.values())
+                                if not existe:
+                                    e = False
+                            break
+
+                        case "0":
+                            print("Cancelando creación de cuenta...")
+                            break
+
+                        case _:
+                            print('Ingresa una Opcion valida')
+                            continue  # <- evitamos seguir si no es válido
+
+                # Si no se generó idProducto, no seguimos
+                if idProducto is None:
+                    continue
+
+                LimpiarConsola()
                 print("Crear Cuenta")
-                numeroCuenta += 499
-                print(f'Número de cuenta: {numeroCuenta}')
+
+                #Datos Solicitados Cliente
+                numeroCuenta = random.randint(1, numeroCuenta)
+                print(f'Tu numero de cuenta general es: {numeroCuenta}')
+                print(f'Tu producto: {producto}, el numero es: {idProducto}')
+
+                cc = input('Ingresa la cedula del titular: ')
                 titular = input("Ingrese el nombre del titular: ")
-                addCuenta(numeroCuenta, titular)
+                email = input('Ingresa el correo del titular: ')
+                edad = int(input('Ingresa la edad del titular: '))
+
+                print('Ingresa el movil y fijo del titular: ')
+                movil = int(input('Ingresa el movil del titular: '))
+                fijo = int(input('Ingresa el fijo del titular: '))
+
+                print('Ingresa los datos de residencia del titular: ')
+                pais = input('Ingresa el pais del titular: ')
+                dep = input('Ingresa el departamento del titular: ')
+                ciudad = input('Ingresa la ciudad del titular: ')
+                direccion = input('Ingresa la Direccion del titular: ')
+
+                estado = "Activo"
+                addCuenta(numeroCuenta, titular, cc, email, edad, movil, fijo, pais, dep, ciudad, direccion,idProducto, producto, estado)
 
                 print(cuentasBancarias)
                 input("Presione Enter para continuar...")
+                LimpiarConsola()
 
             case 2:
+                #Depositar Dinero en una cuenta
+                LimpiarConsola()
                 print("Depositar Dinero")
                 numeroCuenta = int(input("Ingrese el número de cuenta: "))
+
                 if numeroCuenta in cuentasBancarias:
-                    monto = float(input("Ingrese el monto a depositar: "))
-                    cuentasBancarias[numeroCuenta]["saldo"] += monto
-                    print("Depósito realizado con éxito.")
+
+                    print("\nProductos disponibles:")
+                    for idProd, datosProd in cuentasBancarias[numeroCuenta]["Productos"].items():
+                        print(f"  {idProd} - {datosProd['NombreProducto']} (Saldo: {datosProd['Saldo']})")
+
+                    # Pedir producto
+                    idProducto = int(input("\nIngrese el ID del producto al que desea depositar: "))
+
+                    if idProducto in cuentasBancarias[numeroCuenta]["Productos"]:
+                        monto = float(input("Ingrese el monto a depositar: "))
+
+                        # Sumar al saldo
+                        cuentasBancarias[numeroCuenta]["Productos"][idProducto]['Saldo'] += monto
+
+                        print(f"\nDepósito realizado con éxito.")
+                        print(f"Nuevo saldo: {cuentasBancarias[numeroCuenta]['Productos'][idProducto]['Saldo']}")
+                    else:
+                        print("El producto no existe.")
+                else:
+                    print(f'La cuenta {numeroCuenta} No esta asociada')
+
+                input("\nPresione Enter para continuar...")
+                LimpiarConsola()
+
+            case 3:
+                #Solicitud de un Credito, Mirando si es Apto o no para el Credito
+                LimpiarConsola()
+                print('=== Solicitar Crédito ===')
+
+                numeroCuenta = int(input("Ingrese el número de cuenta: "))
+
+                if numeroCuenta not in cuentasBancarias:
+                    print("La cuenta no existe.")
+                    input("Enter para continuar...")
+                    break
+
+                # Mostrar menú de créditos
+                while True:
+                    print('\n¿Qué tipo de crédito deseas solicitar?')
+                    opcion = input('1. Crédito Libre Inversión \n2. Crédito Vivienda \n3. Crédito Compra Automóvil \n0. Salir \nOpción: ')
+                    e = True
+                    idProducto = None
+
+                    match opcion:
+                        case "1":
+                            producto = 'Crédito Libre Inversión'
+                            while e:
+                                idProducto = random.randint(80000, creditoLibreInv)
+                                if not any(idProducto in cuenta["Productos"] for cuenta in cuentasBancarias.values()):
+                                    e = False
+                            break
+                        case "2":
+                            producto = 'Crédito Vivienda'
+                            while e:
+                                idProducto = random.randint(400000, creditoVivienda)
+                                if not any(idProducto in cuenta["Productos"] for cuenta in cuentasBancarias.values()):
+                                    e = False
+                            break
+                        case "3":
+                            producto = 'Crédito Compra Automóvil'
+                            while e:
+                                idProducto = random.randint(100000, creditoCompraAuto)
+                                if not any(idProducto in cuenta["Productos"] for cuenta in cuentasBancarias.values()):
+                                    e = False
+                            break
+                        case "0":
+                            print("Solicitud cancelada.")
+                            continue
+                        case _:
+                            print("Opción inválida.")
+                            continue
+
+                LimpiarConsola()
+                # Datos para evaluación
+                ingresosMensuales = float(input('Ingresa el total de ingresos mensuales: '))
+                montoSolicitado = float(input('Ingresa el monto de crédito solicitado: '))
+                plazoMeses = int(input('Ingresa el plazo (meses) para pagar: '))
+                LimpiarConsola()
+
+                #Calculo del RCI - Relacion Cuota Ingresos
+                cuota = montoSolicitado / plazoMeses
+                RCI = cuota / ingresosMensuales
+
+                print(f"\nEvaluando crédito... (RCI: {RCI:.2f})")
+                time.sleep(1)
+
+                LimpiarConsola()
+                #Evaluacion si es Apto o No
+                if RCI <= 0.4:
+                    print("✅ Crédito aprobado.")
+
+                    # Registrar el crédito en la cuenta
+                    cuentasBancarias[numeroCuenta]["Productos"][idProducto] = {
+                        "NombreProducto": producto,
+                        "Saldo": montoSolicitado,
+                        "Estado": "Activo",
+                        "Historial": {
+                            1: {
+                                "FechaMovimiento": time.strftime("%Y-%m-%d"),
+                                "Valor": montoSolicitado,
+                                "TipoMovimiento": "Crédito Aprobado"
+                            }
+                        }
+                    }
+                    print(f"Crédito {producto} creado con ID {idProducto} y saldo {montoSolicitado}")
+                else:
+                    print("❌ No eres apto para el crédito (RCI mayor a 0.4).")
+
+                input("Enter para continuar...")
+                LimpiarConsola()
+
+            case 4:
+                #Retirar Dinero que tengamos en alguna de las cuentas
+                LimpiarConsola()
+                print("Retirar Dinero")
+                numeroCuenta = int(input("Ingrese el número de cuenta: "))
+
+                #La cuenta existe?
+                if numeroCuenta in cuentasBancarias:
+
+                    print("\nProductos disponibles:")
+                    for idProd, datosProd in cuentasBancarias[numeroCuenta]["Productos"].items():
+                        print(f"  {idProd} - {datosProd['NombreProducto']} (Saldo: {datosProd['Saldo']})")
+
+                    # Pedir producto
+                    idProducto = int(input("\nIngrese el ID del producto al que desea Retirar: "))
+
+                    if idProducto in cuentasBancarias[numeroCuenta]["Productos"]:
+                        monto = float(input("Ingrese el monto a Retirar: "))
+
+                        #Revisa que el saldo de la cuenta, es mayor al monto que desea retirar el cliente
+                        if cuentasBancarias[numeroCuenta]["Productos"][idProducto]['Saldo'] > monto:
+                            cuentasBancarias[numeroCuenta]["Productos"][idProducto]['Saldo'] -= monto
+                            print(f"\nDepósito realizado con éxito.")
+                            print(f"Nuevo saldo: {cuentasBancarias[numeroCuenta]['Productos'][idProducto]['Saldo']}")
+                        else:
+                            print('Saldo insuficiente')
+                            print(f"Saldo: {cuentasBancarias[numeroCuenta]['Productos'][idProducto]['Saldo']}")
+
+                    else:
+                        print("El producto no existe.")
+                else:
+                    print("La cuenta no existe.")
+
+                input("\nPresione Enter para continuar...")
+                LimpiarConsola()
+
+            case 5:
+                #Pago de la cuota del Credito
+                LimpiarConsola()
+                print("Pago de Cuota de Crédito")
+                numeroCuenta = int(input("Ingrese el número de cuenta: "))
+
+                if numeroCuenta in cuentasBancarias:
+                    # Mostrar productos disponibles
+                    print("Productos de la cuenta:")
+                    for idProd, datosProd in cuentasBancarias[numeroCuenta]["Productos"].items():
+                        print(f"ID: {idProd} - {datosProd['Nombre']} - Saldo: {datosProd['Saldo']} - Estado: {datosProd['Estado']}")
+
+                    idProducto = int(input("Ingrese el ID del producto (crédito) a pagar: "))
+
+                    if idProducto in cuentasBancarias[numeroCuenta]["Productos"]:
+                        producto = cuentasBancarias[numeroCuenta]["Productos"][idProducto]
+
+                        if "Credito" in producto["Nombre"]:
+                            monto = float(input("Ingrese el monto a pagar: "))
+
+                            if monto > 0:
+                                if monto >= producto["Saldo"]:
+                                    producto["Saldo"] = 0
+                                    print("Crédito pagado en su totalidad.")
+                                else:
+                                    producto["Saldo"] -= monto
+                                    print(f"Pago realizado. Saldo restante del crédito: {producto['Saldo']}")
+                            else:
+                                print("El monto debe ser mayor a 0.")
+                        else:
+                            print("El producto seleccionado no es un crédito.")
+                    else:
+                        print("El ID del producto no existe.")
                 else:
                     print("La cuenta no existe.")
 
                 input("Presione Enter para continuar...")
-
-            case 3:
-                pass
-
-            case 4:
-                pass
-
-            case 5:
-                pass
+                LimpiarConsola()
 
             case 6:
-                pass
+                #Cancelacion de una de las cuentas del cliente
+                LimpiarConsola()
+                print('Cancelar Cuenta')
+                numeroCuenta = int(input("Ingrese el número de cuenta: "))
+
+                if numeroCuenta in cuentasBancarias:
+                    print("\nProductos disponibles:")
+                    for idProd, datosProd in cuentasBancarias[numeroCuenta]["Productos"].items():
+                        print(f"  {idProd} - {datosProd['NombreProducto']} (Saldo: {datosProd['Saldo']})")
+
+                    idProducto = int(input("\nIngrese el ID del producto al que desea Cancelar: "))
+                    if cuentasBancarias[numeroCuenta]["Productos"][idProducto] == 0:
+                        valorEliminado = cuentasBancarias[numeroCuenta]["Productos"].pop(idProducto)
+                    else:
+                        print('')
+
+                    print(valorEliminado)
+                    print(cuentasBancarias)
+                    input('Enter para continuar')
+
+                LimpiarConsola()
 
             case 0:
                 print("Saliendo del sistema...")
                 break
 
-    except (ValueError, TypeError) as e:
+    except (ValueError, TypeError, KeyboardInterrupt) as e:
         print(f"Error: {e}. Por favor, ingrese un valor válido.")
         continue
